6756f64e1a0f23715e7f1fa98f54bbf13726426b
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-08-06 23:53:40 -0300

    transcricao

---
+++ src/services/gemini.ts
@@ -0,0 +1,31 @@
+import { GoogleGenAI } from '@google/genai'
+import { env } from '../env.ts'
+
+const gemini = new GoogleGenAI({
+  apiKey: env.GEMINI_API_KEY,
+})
+
+const model = 'gemini-2.5-flash'
+
+export async function transcribeAudio(audioAsBase64: string, mimeType: string) {
+  const response = await gemini.models.generateContent({
+    model,
+    contents: [
+      {
+        text: 'Transcreva o áudi para português do Brasil. Seja preciso e natural na transcrição. Mantenha a pontuação adequada e divida o texto em para parágrafos quando for apropriado.',
+      },
+      {
+        inlineData: {
+          mimeType,
+          data: audioAsBase64,
+        },
+      },
+    ],
+  })
+
+  if (!response) {
+    throw new Error('Não foi possivel converter o áudio')
+  }
+
+  return response.text
+}
